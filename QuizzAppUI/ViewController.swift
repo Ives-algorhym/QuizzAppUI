6ec6305dbdfbb526474c4c39688448e7165c3e2b
ives-algorhym <algoives@icloud.com>
2025-10-27 17:37:47 -0700

    Initial Commit

---
+++ QuizzAppUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuizzAppUI
+//
+//  Created by Ives Murillo on 10/27/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
